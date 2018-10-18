e11f779c92199f19571dc21d7d07bdf746875a8c
locky <inox127@hotmail.com>
2018-10-19 00:51:57 +0200

    lines added 5

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let attack1 = document.querySelector(".btn-atk1-norm");
-let changeImg1 = document.querySelector("#atk1norm");
+let changeImg1 = document.querySelector("#zone1norm");
 let changeSizeBtnOnAtk = document.querySelector(".atk1-norm");
 let logStats1 = document.querySelector(".log1");
 
+let changeImg2 = document.querySelector("#zone2norm");
+let logStats2 = document.querySelector(".log2");
 
 /*
 
@@ -32,7 +34,11 @@ function presse(event) {
             changeSizeBtnOnAtk.style.maxWidth = "37px";
             changeSizeBtnOnAtk.style.maxHeight = "37px";
             
+            changeImg2.src = "images/move/elf1movattaked.gif"; 
             break;
+        case 81:
+        case 83:
+        case 87:
  
 
     };
@@ -43,11 +49,18 @@ function relacheBouton(event) {
         case 90:
             changeImg1.src = "images/move/soldat1stand.gif";
             changeSizeBtnOnAtk.style = null;
+            
             let d = new Date();
             el1 = document.createElement("LI");
             el1.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance son attaque A !`;
-            logStats1.appendChild(el1); 
+            let afficheAuTop = logStats1.appendChild(el1); 
+            logStats1.insertBefore(afficheAuTop, logStats1.firstElementChild);
 
+            changeImg2.src = "images/move/elf1stand.gif";
+            el2 = document.createElement("LI");
+            el2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se prend une patate !`;
+            let afficheAuTop2 = logStats2.appendChild(el2); 
+            logStats2.insertBefore(afficheAuTop2, logStats2.firstElementChild);
             break;
         case 40:
             down.classList.remove("highlight");
@@ -63,4 +76,5 @@ function relacheBouton(event) {
 };
 
 document.body.addEventListener("keydown", presse);
+
 document.body.addEventListener("keyup", relacheBouton);
