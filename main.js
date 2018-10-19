11e73e74c8686f137b134d2aba9ac84d8ea4e155
Locky <inox127@hotmail.com>
2018-10-19 13:50:59 +0200

    added lines

--- main.js
+++ main.js
@@ -8,23 +8,14 @@ let logStats1 = document.querySelector(".log1");
 
 let changeImg2 = document.querySelector("#zone2norm");
 let logStats2 = document.querySelector(".log2");
-let d = new Date();
 
-/*
-
-function swapAtk() {
-
-    changeImg1.src = "images/move/soldat1movatk.gif"; 
-}
+let putEffects2 = document.querySelector("#zone2eff");
 
 
+let link = document.querySelector(".atk1-norm");
+link.addEventListener("click", presse);
+console.log(link);
 
-if (attack1.addEventListener){
-    attack1.addEventListener("click", swapAtk());
-}
-if (attack1.removeEventListener){
-    attack1.removeEventListener("click", swapAtk());
-}*/
 
 let up = document.querySelector("#up");
 let down = document.querySelector("#down");
@@ -32,48 +23,56 @@ let left = document.querySelector("#left");
 let right = document.querySelector("#right");
 
 function presse(event) {
-    switch (event.keyCode) { /*preferez event.keyCode � event.charCode ici, sinon �a fout la merde*/
-        case 90:
-            changeImg1.src = "images/move/soldat1movatkA.gif";
-            changeSizeBtnOnAtk.style.maxWidth = "37px";
-            changeSizeBtnOnAtk.style.maxHeight = "37px";
+    if (event.keyCode != null) {
+        switch (event.keyCode) { /*preferez event.keyCode a event.charCode ici, sinon ca fout la merde*/
 
-            changeImg2.src = "images/move/elf1movattaked.gif";
-            break;
-        case 81:
-            changeImg1.src = "images/move/soldat1movatkA.gif";
-            changeSizeBtnOnAtk2.style.maxWidth = "37px";
-            changeSizeBtnOnAtk2.style.maxHeight = "37px";
+            case 90:
+                changeImg1.src = "images/move/soldat1movatkA.gif";
+                changeSizeBtnOnAtk.style.maxWidth = "37px";
+                changeSizeBtnOnAtk.style.maxHeight = "37px";
 
-            changeImg2.src = "images/move/elf1movattaked.gif";
-            break;
-        case 83:
-            changeImg1.src = "images/move/soldat1movatkA.gif";
-            changeSizeBtnOnAtk3.style.maxWidth = "37px";
-            changeSizeBtnOnAtk3.style.maxHeight = "37px";
+                changeImg2.src = "images/move/elf1movattaked.gif";
+                break;
+            case 81:
+                changeImg1.src = "images/move/soldat1movatkA.gif";
+                changeSizeBtnOnAtk2.style.maxWidth = "37px";
+                changeSizeBtnOnAtk2.style.maxHeight = "37px";
 
-            changeImg2.src = "images/move/elf1movattaked.gif";
-            break;
+                changeImg2.src = "images/move/elf1movattaked.gif";
+                break;
+            case 83:
+                changeImg1.src = "images/move/soldat1movatkA.gif";
+                changeSizeBtnOnAtk3.style.maxWidth = "37px";
+                changeSizeBtnOnAtk3.style.maxHeight = "37px";
 
-        case 87:
-            changeImg1.src = "images/move/soldat1movatkA.gif";
-            changeSizeBtnOnAtk4.style.maxWidth = "37px";
-            changeSizeBtnOnAtk4.style.maxHeight = "37px";
+                changeImg2.src = "images/move/elf1movattaked.gif";
 
-            changeImg2.src = "images/move/elf1movattaked.gif";
-            break;
+                putEffects2.src = "images/skills/thunderSkill.gif";
+                break;
+
+            case 87:
+                changeImg1.src = "images/move/soldat1movatkA.gif";
+                changeSizeBtnOnAtk4.style.maxWidth = "37px";
+                changeSizeBtnOnAtk4.style.maxHeight = "37px";
+
+                changeImg2.src = "images/move/elf1movattaked.gif";
+                break;
+
+
+        }
+    } 
 
 
     };
-};
 
 function relacheBouton(event) {
+    let d = new Date();
     switch (event.keyCode) {
         case 90:
             changeImg1.src = "images/move/soldat1stand.gif";
             changeSizeBtnOnAtk.style = null;
 
-            
+
             el1 = document.createElement("LI");
             el1.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance son attaque A !`;
             let afficheAuTop = logStats1.appendChild(el1);
@@ -89,50 +88,52 @@ function relacheBouton(event) {
             changeImg1.src = "images/move/soldat1stand.gif";
             changeSizeBtnOnAtk2.style = null;
 
-            
+
             el2v2 = document.createElement("LI");
             el2v2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance son attaque B !`;
             let afficheAuTop2v2 = logStats1.appendChild(el2v2);
             logStats1.insertBefore(afficheAuTop2v2, logStats1.firstElementChild);
 
             changeImg2.src = "images/move/elf1stand.gif";
-            el3 = document.createElement("LI");
-            el3.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se prend une rouste !`;
-            let afficheAuTop3 = logStats2.appendChild(el3);
-            logStats2.insertBefore(afficheAuTop3, logStats2.firstElementChild);
+            el81 = document.createElement("LI");
+            el81.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se prend une rouste !`;
+            let afficheAuTop81 = logStats2.appendChild(el81);
+            logStats2.insertBefore(afficheAuTop81, logStats2.firstElementChild);
             break;
         case 83:
-            /*changeImg1.src = "images/move/soldat1stand.gif";
+            changeImg1.src = "images/move/soldat1stand.gif";
             changeSizeBtnOnAtk3.style = null;
 
-            
-            el1 = document.createElement("LI");
-            el1.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance son attaque ultime !`;
-            let afficheAuTop = logStats1.appendChild(el1);
-            logStats1.insertBefore(afficheAuTop, logStats1.firstElementChild);
+
+            el3v3 = document.createElement("LI");
+            el3v3.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance son attaque ultime !`;
+            let afficheAuTop3v3 = logStats1.appendChild(el3v3);
+            logStats1.insertBefore(afficheAuTop3v3, logStats1.firstElementChild);
 
             changeImg2.src = "images/move/elf1stand.gif";
-            el2 = document.createElement("LI");
-            el2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 prend cher !`;
-            let afficheAuTop2 = logStats2.appendChild(el2);
-            logStats2.insertBefore(afficheAuTop2, logStats2.firstElementChild);
+            el83 = document.createElement("LI");
+            el83.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 prend cher !`;
+            let afficheAuTop83 = logStats2.appendChild(el83);
+            logStats2.insertBefore(afficheAuTop83, logStats2.firstElementChild);
+
+            putEffects2.removeAttribute("src");
             break;
         case 87:
             changeImg1.src = "images/move/soldat1stand.gif";
             changeSizeBtnOnAtk4.style = null;
 
-            
-            el1 = document.createElement("LI");
-            el1.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance sa defense pour une sec !`;
-            let afficheAuTop = logStats1.appendChild(el1);
-            logStats1.insertBefore(afficheAuTop, logStats1.firstElementChild);
+
+            el4v4 = document.createElement("LI");
+            el4v4.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${ Math.floor(d.getMilliseconds()/10)}] Joueur 1 lance sa defense pour une sec !`;
+            let afficheAuTop4v4 = logStats1.appendChild(el4v4);
+            logStats1.insertBefore(afficheAuTop4v4, logStats1.firstElementChild);
 
             changeImg2.src = "images/move/elf1stand.gif";
-            el2 = document.createElement("LI");
-            el2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se fait une raison...`;
-            let afficheAuTop2 = logStats2.appendChild(el2);
-            logStats2.insertBefore(afficheAuTop2, logStats2.firstElementChild);
-            break;*/
+            el87 = document.createElement("LI");
+            el87.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se fait une raison...`;
+            let afficheAuTop87 = logStats2.appendChild(el87);
+            logStats2.insertBefore(afficheAuTop87, logStats2.firstElementChild);
+            break;
 
     };
 };
