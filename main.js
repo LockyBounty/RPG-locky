ab629db06207ab6fa7341c843721c13103c05469
locky <inox127@hotmail.com>
2018-10-21 21:29:58 +0200

    added functionnal lifebar and first sound atk

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+// DECLARATION DE VARIABLES
+
 let attack1 = document.querySelector(".btn-atk1-norm");
 let changeImg1 = document.querySelector("#zone1norm");
 let changeSizeBtnOnAtk = document.querySelector(".atk1-norm");
 let changeSizeBtnOnAtk2 = document.querySelector(".atk1-moy");
 let changeSizeBtnOnAtk3 = document.querySelector(".atk1-fort");
 let changeSizeBtnOnAtk4 = document.querySelector(".atk1-def");
+
+
+let attackOnHP1 = document.querySelector(".perso1-vie");
+let attackOnHP2 = document.querySelector(".perso2-vie");
+
+
+
+
 let logStats1 = document.querySelector(".log1");
 
 let changeImg2 = document.querySelector("#zone2norm");
@@ -11,7 +21,23 @@ let logStats2 = document.querySelector(".log2");
 
 let changeZone1 = document.querySelector(".zone-perso1");
 
+let putEffects1 = document.querySelector("#zone1eff");
 let putEffects2 = document.querySelector("#zone2eff");
+
+
+
+let lifePoints = 100;
+
+/*function calculPourcentage() {
+    return (chosen1[attack] $ 100)/data_value);--> chosen 1 et data_value a faire
+}*/
+let sonAtkA = document.getElementById("soundAtk1"); 
+
+
+
+
+
+
 
 // BASIC Souris
 const basicAttackButton = document.querySelector(".atk1-norm");
@@ -45,47 +71,48 @@ let right = document.querySelector("#right");
 
 function updateCombatLogs(attackName) {
     let d = new Date();
+    let dateTimeFormattedInFrench = `${d.toLocaleTimeString("fr-FR")}.${Math.floor(d.getMilliseconds() / 10)}`;
     
     switch (attackName) {
         case "basic":
             el1 = document.createElement("LI");
-            el1.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 1 lance son attaque A !`;
+            el1.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque A !`;
             let afficheAuTop = logStats1.appendChild(el1);
             logStats1.insertBefore(afficheAuTop, logStats1.firstElementChild);
             el2 = document.createElement("LI");
-            el2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se prend une patate !`;
+            el2.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 2 se prend une patate !`;
             let afficheAuTop2 = logStats2.appendChild(el2);
             logStats2.insertBefore(afficheAuTop2, logStats2.firstElementChild);
             break;
         case "medium":
             el2v2 = document.createElement("LI");
-            el2v2.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 1 lance son attaque B !`;
+            el2v2.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque B !`;
             let afficheAuTop2v2 = logStats1.appendChild(el2v2);
             logStats1.insertBefore(afficheAuTop2v2, logStats1.firstElementChild);
             el81 = document.createElement("LI");
-            el81.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se prend une rouste !`;
+            el81.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 2 se prend une rouste !`;
             let afficheAuTop81 = logStats2.appendChild(el81);
             logStats2.insertBefore(afficheAuTop81, logStats2.firstElementChild);
             break;
         case "hard":
             el3v3 = document.createElement("LI");
-            el3v3.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 1 lance son attaque ultime !`;
+            el3v3.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque ultime !`;
             let afficheAuTop3v3 = logStats1.appendChild(el3v3);
             logStats1.insertBefore(afficheAuTop3v3, logStats1.firstElementChild);
             el83 = document.createElement("LI");
-            el83.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 prend cher !`;
+            el83.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 2 prend cher !`;
             let afficheAuTop83 = logStats2.appendChild(el83);
             logStats2.insertBefore(afficheAuTop83, logStats2.firstElementChild);
             break;
         case "def":
             el4v4 = document.createElement("LI");
-            el4v4.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 1 lance sa defense pour une sec !`;
+            el4v4.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance sa defense pour une sec !`;
             let afficheAuTop4v4 = logStats1.appendChild(el4v4);
             logStats1.insertBefore(afficheAuTop4v4, logStats1.firstElementChild);
 
             
             el87 = document.createElement("LI");
-            el87.innerHTML = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}.${Math.floor(d.getMilliseconds() / 10)}] Joueur 2 se fait une raison...`;
+            el87.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 2 se fait une raison...`;
             let afficheAuTop87 = logStats2.appendChild(el87);
             logStats2.insertBefore(afficheAuTop87, logStats2.firstElementChild);
             break;
@@ -94,11 +121,31 @@ function updateCombatLogs(attackName) {
 
 }
 
+function playAudio1() {
+    sonAtkA.play();
+    sonAtkA.currentTime = 0.07;
+} 
+
+function pauseAudio1() {
+    sonAtkA.pause();
+   // sonAtkA.currentTime = 0.9;
+}
+
 function basicAttackStart() {
+    
     changeImg1.src = "images/move/soldat1movatkA.gif";
-    // changeZone1.style.animation = "moveChar1 1s linear";
+    
     changeSizeBtnOnAtk.style.maxWidth = "37px";
     changeSizeBtnOnAtk.style.maxHeight = "37px";
+
+
+    playAudio1();
+
+    if (lifePoints > 0) {
+        lifePoints -= 1;
+        attackOnHP2.style.width = `${lifePoints}%`;
+
+    }
     changeImg2.src = "images/move/elf1movattaked.gif";
 }
 
@@ -107,8 +154,21 @@ function basicAttackEnd() {
     changeSizeBtnOnAtk.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
     updateCombatLogs("basic");
-
-}
+    
+    pauseAudio1();
+    affBarVie2();
+    /*si on veut reset le son, mais ca donne un mauvais rendu */
+    if (lifePoints === 0) {
+        changeImg1.src = "images/move/soldat1stand.gif";
+        putEffects1.removeAttribute("src");
+        changeImg2.src = "images/move/elf1dead.gif";
+        setTimeout(onceDead,500);
+        console.log(lifePoints);
+        affBarVie2();
+    }
+    console.log(lifePoints);
+    affBarVie2();
+} 
 
 // MEDIUM ATTACK
 
@@ -117,37 +177,85 @@ function mediumAttackStart() {
     changeSizeBtnOnAtk2.style.maxWidth = "37px";
     changeSizeBtnOnAtk2.style.maxHeight = "37px";
     changeImg2.src = "images/move/elf1movattaked.gif";
+
+    if (lifePoints > 10) {
+        lifePoints -= 10;
+        attackOnHP2.style.width = `${lifePoints}%`;
+    }
+     else {
+        lifePoints = 0;
+        attackOnHP2.style.width = `0%`;
+        changeImg2.src = "images/move/elf1fall.gif";
+    }
+
+    
 }
 function mediumAttackEnd() {
-    let d = new Date();
+    let d = new Date;
     changeImg1.src = "images/move/soldat1stand.gif";
     changeSizeBtnOnAtk2.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
     updateCombatLogs("medium");
+    document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    if (lifePoints === 0) {
+        changeImg1.src = "images/move/soldat1stand.gif";
+        putEffects1.removeAttribute("src");
+        changeImg2.src = "images/move/elf1dead.gif";
+        setTimeout(onceDead, 500);
+        document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    }
+    
 }
 
 // HARD ATTACK 
 function hardAttackStart() {
+    
     changeImg1.src = "images/move/soldat1invoke.gif";
+    putEffects1.src = "images/skills/shield-p1.gif";
+
     changeSizeBtnOnAtk3.style.maxWidth = "37px";
     changeSizeBtnOnAtk3.style.maxHeight = "37px";
+
+    if (lifePoints>35) {
+        lifePoints -= 35;
+        attackOnHP2.style.width = `${lifePoints}%`;
+        
+    } else {
+        lifePoints = 0;
+        attackOnHP2.style.width = `0%`;
+        
+    }
+
     changeImg2.src = "images/move/elf1fall.gif";
-    putEffects2.src = "images/skills/thunderAtkv2.gif";
+    putEffects2.src = "images/skills/thunderAtkv4.gif";
 }
 
 function hardAttackEnd() {
     let d = new Date();
     changeImg1.src = "images/move/soldat1stand.gif";
+    putEffects1.removeAttribute("src");
     changeSizeBtnOnAtk3.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
     putEffects2.removeAttribute("src");
     updateCombatLogs("hard");
+    document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
 
+    if (lifePoints === 0) {
+        changeImg1.src = "images/move/soldat1stand.gif";
+        putEffects1.removeAttribute("src");
+        changeImg2.src = "images/move/elf1dead.gif";
+        confirm("Bravo, vous avez occi l'ennemi !");
+        setTimeout(onceDead, 500);
+        document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    }
+    
 }
 
 //DEFENSE SKILL
 function defenseSkillStart () {
     changeImg1.src = "images/move/soldat1invoke.gif";
+    /*changeZone1.src = "shield-p1.gif";*/
+    
     changeSizeBtnOnAtk4.style.maxWidth = "37px";
     changeSizeBtnOnAtk4.style.maxHeight = "37px";
     
@@ -157,15 +265,42 @@ function defenseSkillEnd () {
     let d = new Date();
     changeImg1.src = "images/move/soldat1stand.gif";
     changeSizeBtnOnAtk4.style = null;
+    
+    updateCombatLogs("def");
 
 }
 
+// DEAD + REZ
+
+function onceDead() {
+    let rez = confirm("Bravo, vous avez vaincu l'ennemi !\nVoulez-vous le soigner ?");
+    if (rez === true) {
+        lifePoints = 100;
+        attackOnHP2.style.width = `${lifePoints}%`;
+        
+        changeImg2.src = "images/move/elf1stand.gif";
+    }
+    
+}
+
+// AFFICHER POURCENTAGE BAR 
+
+function affBarVie2() {
+
+
+return document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+
+}
+
+
 function presse(event) {
-    console.log(event.keyCode);
+    /*console.log(event.keyCode);*/
     if (event.keyCode != null) {
-        switch (event.keyCode) { /*preferez event.keyCode a event.charCode ici, sinon ca fout la merde*/
+        switch (event.keyCode) { 
             case 90:
+                
                 basicAttackStart();
+                
                 break;
             case 81:
                 mediumAttackStart();
@@ -186,10 +321,12 @@ function presse(event) {
 
 function relacheBouton(event) {
     let d = new Date();
-    console.log(event.keyCode);
+    /*console.log(event.keyCode);*/
+    
     switch (event.keyCode) {
         case 90:
             basicAttackEnd();
+            
             break;
         case 81:
             mediumAttackEnd();
@@ -203,6 +340,9 @@ function relacheBouton(event) {
 
     };
 };
+
+
+
 
 document.body.addEventListener("keydown", presse);
 
