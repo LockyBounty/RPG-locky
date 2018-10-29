ae9639bdbefbd60d4c396c91377132955f6c4f06
locky <inox127@hotmail.com>
2018-10-29 02:22:02 +0100

    added upgrade box and char changing with selection and stamina bar

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 // DECLARATION DE CLASSE ET OBJETS
 
 class Hero {
-    constructor(name, job, weapon) {
+    constructor(name, job, weapon, frame, movimg) {
         this.name = name;
         this.job = job;
         this.weapon = weapon;
+        this.frame = frame;
+        this.movimg = movimg;
         this.lifeP = 100;
         this.attackP = 15;
         this.defenceP = 30;
+        this.staminaP = 100;
         this.lvl = 1;
         this.expP = 0;
     }
 }
 
 
-let hero1 = new Hero("Jeanne", "Chevalière", "Claymore");
-let hero2 = new Hero("Catherine", "Pyromancienne", "Pyro-Spiro");
-let hero3 = new Hero("Eleanor", "Prêtresse", "Natura");
-let hero4 = new Hero("Nathanael", "Valkyrie", "Galadbolg");
-let hero5 = new Hero("Galathe", "Combattante", "Poings-de-fer");
+let hero1 = new Hero("Jeanne", "Chevalière", "Theudericus", "images/soldat1.png", "images/move/soldat1stand.gif");
+let hero2 = new Hero("Catherine", "Pyromancienne", "Pyro-Spiro", "images/mago1.png", "images/move/mago1stand.gif");
+let hero3 = new Hero("Eleanor", "Prêtresse", "Natura", "images/elf1.png", "images/move/elf1stand.gif");
+let hero4 = new Hero("Nathanael", "Valkyrie", "Galadbolg", "images/ange1.png","images/move/ange1stand.gif");
+let hero5 = new Hero("Galathe", "Combattante", "Poings-de-fer", "images/fighter1.png", "images/move/fighter1stand.gif");
 
 let hero = [hero1, hero2, hero3, hero4, hero5];
 
@@ -28,6 +31,8 @@ let hero = [hero1, hero2, hero3, hero4, hero5];
 
 
 // DECLARATION DE VARIABLES
+
+let cptGlobal = 0;
 
 let attack1 = document.querySelector(".btn-atk1-norm");
 let changeImg1 = document.querySelector("#zone1norm");
@@ -40,7 +45,7 @@ let changeSizeBtnOnAtk4 = document.querySelector(".atk1-def");
 let attackOnHP1 = document.querySelector(".perso1-vie");
 let attackOnHP2 = document.querySelector(".perso2-vie");
 
-
+let attackStamina1 = document.querySelector(".perso1-stamina");
 
 
 let logStats1 = document.querySelector(".log1");
@@ -54,8 +59,8 @@ let putEffects1 = document.querySelector("#zone1eff");
 let putEffects2 = document.querySelector("#zone2eff");
 let putEffects2v2 = document.querySelector("#zone2-1eff");
 
-
 let lifePoints = 100;
+let lifePointsEnnemy = 100;
 
 /*function calculPourcentage() {
     return (chosen1[attack] $ 100)/data_value);--> chosen 1 et data_value a faire
@@ -65,7 +70,7 @@ let sonAtkB = document.getElementById("soundAtk2");
 let sonAtkC = document.getElementById("soundAtk3");
 let sonAtkD = document.getElementById("soundAtk4");
 
-document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
 
 let charArray = ["images/soldat1.png", "images/mago1.png", "images/elf1.png", "images/ange1.png", "images/fighter1.png"];
 let selectPlus = document.querySelector("#selectionPlus");
@@ -79,7 +84,10 @@ let selectionMenuB = document.querySelector(".infos-aff");
 let boutonSelectionShop = document.querySelector(".shop");
 let selectionMenuC = document.querySelector(".shop-aff");
 
+let initialStamina = hero[cptGlobal].staminaP;
 
+let frameP1 = document.querySelector("#img1");
+let frameP2;
 
 // BASIC Souris
 
@@ -119,7 +127,7 @@ function updateCombatLogs(attackName) {
     switch (attackName) {
         case "basic":
             el1 = document.createElement("LI");
-            el1.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque A !`;
+            el1.innerHTML = `[${dateTimeFormattedInFrench}] ${hero[cptGlobal].name} lance son attaque A !`;
             let afficheAuTop = logStats1.appendChild(el1);
             logStats1.insertBefore(afficheAuTop, logStats1.firstElementChild);
             el2 = document.createElement("LI");
@@ -129,7 +137,7 @@ function updateCombatLogs(attackName) {
             break;
         case "medium":
             el2v2 = document.createElement("LI");
-            el2v2.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque B !`;
+            el2v2.innerHTML = `[${dateTimeFormattedInFrench}] ${hero[cptGlobal].name} attaque avec ${hero[cptGlobal].weapon} et inflige <font face="Chakra" color ="gold">${hero[cptGlobal].attackP}</font> points de dégats !`;
             let afficheAuTop2v2 = logStats1.appendChild(el2v2);
             logStats1.insertBefore(afficheAuTop2v2, logStats1.firstElementChild);
             el81 = document.createElement("LI");
@@ -139,7 +147,7 @@ function updateCombatLogs(attackName) {
             break;
         case "hard":
             el3v3 = document.createElement("LI");
-            el3v3.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance son attaque ultime !`;
+            el3v3.innerHTML = `[${dateTimeFormattedInFrench}] ${hero[cptGlobal].name} lance son attaque ultime !`;
             let afficheAuTop3v3 = logStats1.appendChild(el3v3);
             logStats1.insertBefore(afficheAuTop3v3, logStats1.firstElementChild);
             el83 = document.createElement("LI");
@@ -149,7 +157,7 @@ function updateCombatLogs(attackName) {
             break;
         case "def":
             el4v4 = document.createElement("LI");
-            el4v4.innerHTML = `[${dateTimeFormattedInFrench}] Joueur 1 lance sa barriere de protection !`;
+            el4v4.innerHTML = `[${dateTimeFormattedInFrench}] ${hero[cptGlobal].name} lance sa barriere de protection !`;
             let afficheAuTop4v4 = logStats1.appendChild(el4v4);
             logStats1.insertBefore(afficheAuTop4v4, logStats1.firstElementChild);
 
@@ -215,25 +223,26 @@ function basicAttackStart() {
 
     playAudio1();
 
-    if (lifePoints > 0) {
-        lifePoints -= 1;
-        attackOnHP2.style.width = `${lifePoints}%`;
+    if (lifePointsEnnemy > 0) {
+        lifePointsEnnemy -= 1;
+        attackOnHP2.style.width = `${lifePointsEnnemy}%`;
 
     }
     changeImg2.src = "images/move/elf1movattaked.gif";
+    updateCombatLogs("basic");
 }
 
 function basicAttackEnd() {
-    changeImg1.src = "images/move/soldat1stand.gif";
+    changeImg1.src = `${hero[cptGlobal].movimg}`;
     changeSizeBtnOnAtk.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
-    updateCombatLogs("basic");
+    
 
     pauseAudio1();
 
     /*si on veut reset le son, mais ca donne un mauvais rendu */
-    if (lifePoints === 0) {
-        changeImg1.src = "images/move/soldat1stand.gif";
+    if (lifePointsEnnemy === 0) {
+        changeImg1.src = `${hero[cptGlobal].movimg}`;
         putEffects1.removeAttribute("src");
         changeImg2.src = "images/move/elf1dead.gif";
         setTimeout(onceDead, 500);
@@ -247,42 +256,48 @@ function basicAttackEnd() {
 // MEDIUM ATTACK
 
 function mediumAttackStart() {
-    changeImg1.src = "images/move/soldat1movatkA.gif";
-    changeSizeBtnOnAtk2.style.maxWidth = "37px";
-    changeSizeBtnOnAtk2.style.maxHeight = "37px";
-    putEffects2v2.src = "images/skills/atk-moy1.gif";
-    changeImg2.src = "images/move/elf1movattaked.gif";
-    playAudio2();
+    if (initialStamina >9) {
+        initialStamina -= 10;
+        attackStamina1.style.width = `${initialStamina}%`;
+        console.log(`${initialStamina}`);
 
-    if (lifePoints > 10) {
-        lifePoints -= 10;
-        attackOnHP2.style.width = `${lifePoints}%`;
+        changeImg1.src = "images/move/soldat1movatkA.gif";
+        changeSizeBtnOnAtk2.style.maxWidth = "37px";
+        changeSizeBtnOnAtk2.style.maxHeight = "37px";
+        putEffects2v2.src = "images/skills/atk-moy1.gif";
+        changeImg2.src = "images/move/elf1movattaked.gif";
+        playAudio2();
+
+
+    if (lifePointsEnnemy > 10) {
+        lifePointsEnnemy -= 10;
+        attackOnHP2.style.width = `${lifePointsEnnemy}%`;
     } else {
-        lifePoints = 0;
+        lifePointsEnnemy = 0;
         attackOnHP2.style.width = `0%`;
         changeImg2.src = "images/move/elf1fall.gif";
     }
-
-
+    updateCombatLogs("medium");
+    }
 }
 
 function mediumAttackEnd() {
 
-    changeImg1.src = "images/move/soldat1stand.gif";
+    changeImg1.src = `${hero[cptGlobal].movimg}`;
     changeSizeBtnOnAtk2.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
-    updateCombatLogs("medium");
+    
     putEffects2v2.removeAttribute("src");
-    document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
     pauseAudio2();
 
-    if (lifePoints === 0) {
-        changeImg1.src = "images/move/soldat1stand.gif";
+    if (lifePointsEnnemy === 0) {
+        changeImg1.src = `${hero[cptGlobal].movimg}`;
         putEffects1.removeAttribute("src");
         putEffects2v2.removeAttribute("src");
         changeImg2.src = "images/move/elf1dead.gif";
         setTimeout(onceDead, 500);
-        document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+        document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
     }
 
 }
@@ -296,61 +311,66 @@ function hardAttackStart() {
     changeSizeBtnOnAtk3.style.maxWidth = "37px";
     changeSizeBtnOnAtk3.style.maxHeight = "37px";
     playAudio3();
-    if (lifePoints > 35) {
-        lifePoints -= 35;
-        attackOnHP2.style.width = `${lifePoints}%`;
+    if (lifePointsEnnemy > 35) {
+        lifePointsEnnemy -= 35;
+        attackOnHP2.style.width = `${lifePointsEnnemy}%`;
 
     } else {
-        lifePoints = 0;
+        lifePointsEnnemy = 0;
         attackOnHP2.style.width = `0%`;
 
     }
 
     changeImg2.src = "images/move/elf1fall.gif";
     putEffects2.src = "images/skills/thunderAtkv4.gif";
+    updateCombatLogs("hard");
 }
 
 function hardAttackEnd() {
 
-    changeImg1.src = "images/move/soldat1stand.gif";
+    changeImg1.src = `${hero[cptGlobal].movimg}`;
     putEffects1.removeAttribute("src");
     changeSizeBtnOnAtk3.style = null;
     changeImg2.src = "images/move/elf1stand.gif";
     putEffects2.removeAttribute("src");
-    updateCombatLogs("hard");
-    document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    
+    document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
     pauseAudio3();
 
-    if (lifePoints === 0) {
-        changeImg1.src = "images/move/soldat1stand.gif";
+    if (lifePointsEnnemy === 0) {
+        changeImg1.src = `${hero[cptGlobal].movimg}`;
         putEffects1.removeAttribute("src");
         changeImg2.src = "images/move/elf1dead.gif";
-        confirm("Bravo, vous avez occi l'ennemi !");
+        /*confirm("Bravo, vous avez occi l'ennemi !");*/
         setTimeout(onceDead, 500);
-        document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+        document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
     }
 
 }
 
 //DEFENSE SKILL
 function defenseSkillStart() {
-    changeImg1.src = "images/move/soldat1invoke.gif";
-    putEffects1.src = "images/skills/shield-p2.gif";
+    if (initialStamina > 19) {
+        initialStamina -= 20;
+        attackStamina1.style.width = `${initialStamina}%`;
 
-    changeSizeBtnOnAtk4.style.maxWidth = "37px";
-    changeSizeBtnOnAtk4.style.maxHeight = "37px";
-    playAudio4();
+        changeImg1.src = "images/move/soldat1invoke.gif";
+        putEffects1.src = "images/skills/shield-p2.gif";
 
+        changeSizeBtnOnAtk4.style.maxWidth = "37px";
+        changeSizeBtnOnAtk4.style.maxHeight = "37px";
+        updateCombatLogs("def");
+        playAudio4();
+    }
 }
 
 function defenseSkillEnd() {
 
-    changeImg1.src = "images/move/soldat1stand.gif";
+    changeImg1.src = `${hero[cptGlobal].movimg}`;
 
     putEffects1.removeAttribute("src");
     changeSizeBtnOnAtk4.style = null;
-
-    updateCombatLogs("def");
+    
     pauseAudio4();
 }
 
@@ -359,9 +379,9 @@ function defenseSkillEnd() {
 function onceDead() {
     let rez = confirm("Excellent, vous avez vaincu l'ennemi !\nVoulez-vous le soigner ?");
     if (rez === true) {
-        lifePoints += 100;
-        attackOnHP2.style.width = `${lifePoints}%`;
-        document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+        lifePointsEnnemy += 100;
+        attackOnHP2.style.width = `${lifePointsEnnemy}%`;
+        document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
         changeImg2.src = "images/move/elf1stand.gif";
     }
 
@@ -370,8 +390,25 @@ function onceDead() {
 // AFFICHER POURCENTAGE BAR 
 
 function affBarVie2() {
-    return document.querySelector("#pv2").innerHTML = `${lifePoints}%`;
+    return document.querySelector("#pv2").innerHTML = `${lifePointsEnnemy}%`;
 }
+
+// REGEN STAMINA 
+
+
+function regenA() {
+    if (initialStamina < hero[cptGlobal].staminaP) {
+        (initialStamina)++;
+    }
+else {
+    clearInterval();
+    }
+    attackStamina1.style.width = `${initialStamina}%`;
+}
+
+setInterval(regenA, 200);
+
+
 
 // AFFICHER MENUS ; attention aux z-index a cause de la superposition
 
@@ -426,11 +463,13 @@ let affMenuShop = () => {
 
 // PLACER IMAGES SELECTION BOX
 
-let cptGlobal = 0;
+
 
 let placeAllInOne = () => {
 selectFrame.src = `${charArray[cptGlobal]}`;
-nameJob.innerHTML = `${hero[cptGlobal].name} / ${hero[cptGlobal].job}`;
+    nameJob.innerHTML = `${hero[cptGlobal].name} / ${hero[cptGlobal].job}`;
+    frameP1.src = `${hero[cptGlobal].frame}`;
+    changeImg1.src = `${hero[cptGlobal].movimg}`;
 document.querySelector(".perso1-nom").innerHTML = `${hero[cptGlobal].name}`;
 document.querySelector(".stat-row1").innerHTML = `${hero[cptGlobal].name}`;
 document.querySelector("#lp-row2").innerHTML = `${hero[cptGlobal].lifeP}`;
@@ -447,8 +486,10 @@ let placerImagesPlus = () => {
         cptGlobal = 0;
     }
     selectFrame.src = `${charArray[cptGlobal]}`;
+    
     if (cptGlobal !== charArray.length - 1) {
         nameJob.innerHTML = `${hero[cptGlobal].name} / ${hero[cptGlobal].job}`; /*Accede au tableau avec les objets "hero"*/
+       
     } else {
         nameJob.innerHTML = `${hero[cptGlobal].name}`;
     }
@@ -517,7 +558,7 @@ function relacheBouton(event) {
 
 
 placeAllInOne();
-
+regenA();
 
 document.body.addEventListener("keydown", presse);
 
